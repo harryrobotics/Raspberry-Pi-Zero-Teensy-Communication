7c9a779b711479c021dfcadda36ade35803dd49f
turtlebot <vanduong_nguyen@mymail.sutd.edu.sg>
2018-07-26 21:09:05 -0700

    modify

--- camera_client.sh
+++ camera_client.sh
@@ -1,3 +1,1 @@
-!#/bin/bash
-
 nc -l 2222 | mplayer -fps 200 -demuxer h264es -
--- camera_pi.sh
+++ camera_pi.sh
@@ -1,2 +1,5 @@
+!#/bin/bash
 
-/opt/vc/bin/raspivid -t 0 -w 300 -h 300 -hf -fps 20 -o - | nc <IP-OF-THE-CLIENT> 2222
+IP_OF_CLIENT=10.19.102.156
+
+raspivid -t 0 -w 300 -h 300 -hf -fps 20 -o - | nc $IP_OF_CLIENT 2222
